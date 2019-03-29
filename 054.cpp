394250cb8c2137c9331042b9948a4d288167ca67
wangsiping97 <wangsiping1997@gmail.com>
2019-03-30 03:02:00 +0800

    Spiral Matrix (beats 100%, beats 100%)

--- 054.cpp
+++ 054.cpp
@@ -24,31 +24,40 @@ using namespace std;
 class Solution {
 public:
     vector<int> spiralOrder(vector< vector<int> >& matrix) {
+	
         vector<int> ans;
-        int i=0, j=0, p=1;
+        
         int m = matrix.size();
         if (m == 0) return ans;
+        if (m == 1) return matrix[0];
         int n = matrix[0].size();
         if (n == 0) return ans;
-        while (p < n || p < m) {
-            while (i < n-p) {
-                ans.push_back(matrix[j][i]);
-                i++;
-            }
-            while (j < m-p) {
-                ans.push_back(matrix[j][i]);
-                j++;
-            }
-            while (i > p-1) {
-                ans.push_back(matrix[j][i]);
-                i--;
-            }
-            while (j > p) {
-                ans.push_back(matrix[j][i]);
-                j--;
-            }
+        if (n == 1) {
+            for (int i = 0; i < m; ans.push_back(matrix[i][0]),i++);
+            return ans;
+        }
+		
+		int i=0, j=0, p=1;
+        int i_ = i, j_ = j;
+		
+        while (p <= n && p <= m) {
+            for (; i < n-p; ans.push_back(matrix[j][i]), i++);
+            if (i_ == i) break;
+            i_ = i;
+            for (; j < m-p; ans.push_back(matrix[j][i]), j++);
+            if (j == j_) break;
+            j_ = j;
+            for (; i > p-1; ans.push_back(matrix[j][i]), i--);
+            if (i == i_) break;
+            i_ = i;
+            for (; j > p; ans.push_back(matrix[j][i]), j--);
+            if (j_ == j) break;
+            j_ = j;
             p++;
         }
+        ans.push_back(matrix[j][i]);
+		
         return ans;
+		
     }
 };
