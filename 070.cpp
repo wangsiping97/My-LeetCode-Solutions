3978ab01fba9280b629bba3eaea839c93522370f
wangsiping97 <wangsiping1997@gmail.com>
2019-07-29 14:40:25 +0800

    Climbing Stairs (beats 100%)

---
+++ 070.cpp
@@ -0,0 +1,16 @@
+// You are climbing a stair case. It takes n steps to reach to the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        int a = 1, b = 2;
+        for (int i = 2; i < n; i++) {
+            int t = b;
+            b += a;
+            a = t;
+        }
+        return b;
+    }
+};
