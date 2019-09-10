e578c620063384f93bd4947472c267e39b9a98ec
wangsiping97 <wangsiping1997@gmail.com>
2019-09-10 23:34:19 +0800

    Search a 2D Matrix (beats 99.80%)

---
+++ 072.cpp
@@ -0,0 +1,16 @@
+// Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+// You have the following 3 operations permitted on a word:
+// Insert a character
+// Delete a character
+// Replace a character
+
+#include <string> 
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        
+    }
+};
