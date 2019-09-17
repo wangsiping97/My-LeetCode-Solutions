c13a36451f7764c167f5cb6c4780e4620a18c9a5
wangsiping97 <wangsiping1997@gmail.com>
2019-09-17 17:14:21 +0800

    74. Search a 2D Matrix (beats 99.80%)

--- 074.cpp
+++ 074.cpp
@@ -8,21 +8,21 @@ using namespace std;
 
 class Solution {
 public:
-    bool binomialSearch(vector<int>& num, int left, int right, int target) {
-        if (left >= right && num[left] != target) return false;
-        int mid = (left + right) / 2;
-        if (target > num[mid]) return binomialSearch(num, mid + 1, right, target);
-        if (target < num[mid]) return binomialSearch(num, left, mid - 1, target);
-        else return true;
-    }
     bool searchMatrix(vector< vector<int> >& matrix, int target) {
         int m = matrix.size();
         if (m == 0) return false;
         int n = matrix[0].size(); // n >= 1
         if (n == 0) return false;
-        int i = 0; 
-        while (i < m && matrix[i][0] <= target) i++;
-        if (i == 0) return false;
-        return binomialSearch(matrix[i - 1], 0, n - 1, target);
+        int o = 0, p = n * m - 1, mid;
+        while (o <= p) {
+            int i, j;
+            mid = (o + p) >> 1;
+            j = mid % n;
+            i = (mid - j) /n;
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] < target) o = mid + 1;
+            else p = mid - 1;
+        }
+        return false;
     }
 };
