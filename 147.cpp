b0df2ac42677f65825f40d5603bf67fb34873383
wangsiping97 <wangsiping1997@gmail.com>
2020-05-15 14:47:20 +0800

    Merge Sort Linked List

---
+++ 147.cpp
@@ -0,0 +1,53 @@
+// Sort a linked list using insertion sort.
+
+#include <iostream> 
+using namespace std; 
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeSortList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode *pfast = head, *pslow = head;
+        ListNode *temp = head;
+        while (pfast != NULL && pfast->next != NULL) {
+            pfast = pfast->next->next; 
+            temp = pslow;
+            pslow = pslow->next;
+        }
+        ListNode* newHead = pslow;
+        temp->next = NULL;
+        return merge(mergeSortList(head), mergeSortList(newHead));
+    }
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode *p1 = list1, *p2 = list2;
+        ListNode* head = new ListNode();
+        ListNode* node = head;
+        while (p1 && p2) {
+            ListNode* temp = new ListNode();
+            if (p1->val < p2->val) {
+                temp->val = p1->val;
+                p1 = p1->next;
+            }
+            else {
+                temp->val = p2->val; 
+                p2 = p2->next;
+            }
+            node->next = temp;
+            node = node->next;
+        }
+        if (p1) node->next = p1;
+        if (p2) node->next = p2;
+        return head->next; 
+    }
+};
