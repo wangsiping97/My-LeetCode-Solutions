47b5371cfda52ca7cab3053e459f2a8fd67f8941
wangsiping97 <wangsiping1997@gmail.com>
2020-05-23 02:57:26 +0800

    Invert Binary Tree (faster than 100%, less than 100%)

---
+++ 226.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <vector> 
+#include <deque> 
+using namespace std; 
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) return root; 
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        root->left = invertTree(right);
+        root->right = invertTree(left);
+        return root;
+    }
+};
