300cc515b9ee9eae23441d2808887b7272b085b2
wangsiping97 <wangsiping1997@gmail.com>
2020-05-23 23:57:42 +0800

    Minimum Cost Tree From Leaf Values (faster than 100%, less than 100%)

--- 1130.cpp
+++ 1130.cpp
@@ -5,11 +5,45 @@
 // Among all possible binary trees considered, return the smallest possible sum of the values of each non-leaf node.  It is guaranteed this sum fits into a 32-bit integer.
 
 #include <vector> 
+#include <cstdlib> 
 using namespace std; 
 
 class Solution {
 public:
     int mctFromLeafValues(vector<int>& arr) {
-        
+        int n = arr.size();
+        int** dp = (int**)malloc(n * sizeof(int*));
+        for (int i = 0; i < n; ++i) {
+            dp[i] = (int*)malloc(n * sizeof(int));
+            memset(dp[i], 0, n * sizeof(int));
+        }
+        int** max = (int**)malloc(n * sizeof(int*));
+        for (int i = 0; i < n; ++i) {
+            max[i] = (int*)malloc(n * sizeof(int));
+            memset(max[i], 0, n * sizeof(int));
+        }
+
+        for (int diff = 0; diff < n; ++diff) {
+            for (int start = 0; start + diff < n; ++start) {
+                int end = start + diff;
+                if (diff == 0) {
+                    dp[start][end] = 0;
+                    max[start][end] = arr[start];
+                    continue;
+                }
+                max[start][end] = arr[end] > max[start][end - 1] ? arr[end] : max[start][end - 1];
+                if (diff == 1) {
+                    dp[start][end] = arr[start] * arr[end];
+                    continue;
+                }
+                int min = INT_MAX; 
+                for (int k = start; k < end; ++k) {
+                    int temp = dp[start][k] + dp[k + 1][end] + max[start][k] * max[k + 1][end];
+                    if (min > temp) min = temp;
+                }
+                dp[start][end] = min;
+            }
+        }
+        return dp[0][n - 1];
     }
 };
