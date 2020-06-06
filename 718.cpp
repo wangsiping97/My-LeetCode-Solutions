96bee75c0cf4fe74c6415fac8b9ac487c9649a72
wangsiping97 <wangsiping1997@gmail.com>
2020-06-06 19:32:54 +0800

    Maximum Length of Repeated Subarray

--- 718.cpp
+++ 718.cpp
@@ -7,30 +7,18 @@ class Solution {
 public:
     int findLength(vector<int>& A, vector<int>& B) {
         int m = A.size(), n = B.size();
-        int dp[n][n];
-        for (int diff = 0; diff < n; ++diff) {
-            for (int i = 0; i + diff < n; ++i) {
-                int j = i + diff;
-                if (isIn(A, B, i, j)) dp[i][j] = diff + 1; 
-                else dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+        int dp[m][n];
+        int res = 0;
+        // dp[i][j]: maximum length of subarray in A[0:i] and B[0:j] 
+        // and **the subarray ends with A[i] and B[j]**
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) dp[i][j] = A[i] == B[j] ? 1 : 0;
+                else if (A[i] != B[j]) dp[i][j] = 0;
+                else dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > res) res = dp[i][j];
             }
         }
-        return dp[0][n - 1];
-    }
-    bool isIn(vector<int>& A, vector<int>& B, int i, int j) {
-        int pa = 0;
-        for (; pa < A.size() - j + i; pa++) {
-            if (A[pa] == B[i]) {
-                bool flag = true;
-                for (int k = 0; i + k <= j; ++k) {
-                    if (A[pa + k] != B[i + k]) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) return true;
-            }
-        }
-        return false;
+        return res;
     }
 };
