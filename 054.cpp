98f8d2ec46fa215d69a88e733cea5e8294d411ac
Siping Wang <wangsiping1997@gmail.com>
2020-11-08 07:38:53 +0800

    Spiral Matrix (beats 100%)

--- 054.cpp
+++ 054.cpp
@@ -17,47 +17,37 @@
 // ]
 // Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
-#include <iostream> 
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
     vector<int> spiralOrder(vector< vector<int> >& matrix) {
-	
+        int flag = 1; // go
         vector<int> ans;
-        
         int m = matrix.size();
         if (m == 0) return ans;
-        if (m == 1) return matrix[0];
         int n = matrix[0].size();
-        if (n == 0) return ans;
-        if (n == 1) {
-            for (int i = 0; i < m; ans.push_back(matrix[i][0]),i++);
-            return ans;
+        int left = 0, right = n - 1, up = 0, down = m - 1;
+        int count = 0;
+        while (1) {
+            for (int j = left; j <= right; ++j) ans.push_back(matrix[up][j]);
+            count += right - left + 1;
+            if (count == m * n) break;
+            up++;
+            for (int i = up; i <= down; ++i) ans.push_back(matrix[i][right]);
+            count += down - up + 1;
+            if (count == m * n) break;
+            right--;
+            for (int j = right; j >= left; --j) ans.push_back(matrix[down][j]);
+            count += right - left + 1;
+            if (count == m * n) break;
+            down--;
+            for (int i = down; i >= up; --i) ans.push_back(matrix[i][left]);
+            count += down - up + 1;
+            if (count == m * n) break;
+            left++;
         }
-		
-		int i=0, j=0, p=1;
-        int i_ = i, j_ = j;
-		
-        while (p <= n && p <= m) {
-            for (; i < n-p; ans.push_back(matrix[j][i]), i++);
-            if (i_ == i) break;
-            i_ = i;
-            for (; j < m-p; ans.push_back(matrix[j][i]), j++);
-            if (j == j_) break;
-            j_ = j;
-            for (; i > p-1; ans.push_back(matrix[j][i]), i--);
-            if (i == i_) break;
-            i_ = i;
-            for (; j > p; ans.push_back(matrix[j][i]), j--);
-            if (j_ == j) break;
-            j_ = j;
-            p++;
-        }
-        ans.push_back(matrix[j][i]);
-		
         return ans;
-		
     }
 };
