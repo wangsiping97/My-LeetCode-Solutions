0007e11554124339f4c6f579fa6b0f7a6e12b88f
Siping Wang <wangsiping1997@gmail.com>
2020-11-13 08:29:46 +0800

    Top K Frequent Elements

---
+++ 337.cpp
@@ -0,0 +1,20 @@
+// The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+// Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+#include <cstdlib> 
+using namespace std; 
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        
+    }
+};
