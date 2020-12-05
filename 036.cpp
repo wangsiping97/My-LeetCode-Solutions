74aba078bd6d22d068c3cf625ff61838b93c6430
Siping Wang <wangsiping1997@gmail.com>
2020-12-05 21:01:32 +0800

    Valid Sudoku

--- 036.cpp
+++ 036.cpp
@@ -10,44 +10,21 @@ using namespace std;
 
 class Solution {
 public:
-    int index(int i, int j) {
-        int ind;
-        if (i >= 0 && i <= 2 && j >= 0 && j <= 2)
-            ind = 18;
-        else if (i >= 0 && i <= 2 && j >= 3 && j <= 5)
-            ind = 19;
-        else if (i >= 0 && i <= 2 && j >= 6 && j <= 8)
-            ind = 20;
-        else if (i >= 3 && i <= 5 && j >= 0 && j <= 2)
-            ind = 21;
-        else if (i >= 3 && i <= 5 && j >= 3 && j <= 5)
-            ind = 22;
-        else if (i >= 3 && i <= 5 && j >= 6 && j <= 8)
-            ind = 23;
-        else if (i >= 6 && i <= 8 && j >= 0 && j <= 2)
-            ind = 24;
-        else if (i >= 6 && i <= 8 && j >= 3 && j <= 5)
-            ind = 25;
-        else
-            ind = 26;
-        return ind;
-    }
-    bool isValidSudoku(vector< vector<char> >& board) {
-        int bucket[27][9];
-        for (int i = 0; i < 27; i++) {
-            for (int j = 0; j < 9; j++)
-                bucket[i][j] = 0;
-        }
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] >= '1' && board[i][j] <= '9')
-                {
-                    bucket[i][board[i][j] - '0' - 1]++;
-                    if (bucket[i][board[i][j] - '0' - 1] == 2) return false;
-                    bucket[j + 9][board[i][j] - '0' - 1]++;
-                    if (bucket[j + 9][board[i][j] - '0' - 1] == 2) return false;
-                    bucket[index(i, j)][board[i][j] - '0' - 1]++;
-                    if (bucket[index(i, j)][board[i][j] - '0' - 1] == 2) return false;
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][9];
+        int col[9][9];
+        int block[9][9];
+        memset(row, 0, 81 * sizeof(int));
+        memset(col, 0, 81 * sizeof(int));
+        memset(block, 0, 81 * sizeof(int));
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] != '.') {
+                    if (++row[i][board[i][j] - '1'] >= 2) return false;
+                    if (++col[board[i][j] - '1'][j] >= 2) return false;
+                    int block_i = i / 3;
+                    int block_j = j / 3;
+                    if (++block[block_i * 3 + block_j][board[i][j] - '1'] >= 2) return false;
                 }
             }
         }
