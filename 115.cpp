7ee675ef845735700489b578ced23e8d08c5d46f
Siping Wang <wangsiping1997@gmail.com>
2020-12-08 17:17:19 +0800

    Distinct Subsequences (faster than 100%)

--- 115.cpp
+++ 115.cpp
@@ -1,5 +1,6 @@
-// Given a string S and a string T, count the number of distinct subsequences of S which equals T.
-// A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+// Given two strings s and t, return the number of distinct subsequences of s which equals t.
+// A string's subsequence is a new string formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+// It's guaranteed the answer fits on a 32-bit signed integer.
 
 #include <string> 
 using namespace std; 
@@ -7,31 +8,28 @@ using namespace std;
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int m = s.length(), n = t.length();
-        if (m == 0 || n == 0) return 0;
-        int ans[m + 1][n + 1];
-        for (int i = 0; i < m + 1; ++i) {
-            memset(ans[i], -1, (n + 1) * 4);
+        int ls = s.length(), lt = t.length();
+        if (ls == 0 || lt == 0) return 0;
+        long dp[ls + 1][lt + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = s[0] == t[0] ? 1 : 0;
+        for (int i = 1; i < ls + 1; ++i) {
+            dp[i][0] = s[i] == t[0] ? 1 + dp[i - 1][0] : dp[i - 1][0];
         }
-        ans[0][0] = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n + 1; ++j) {
-                if (ans[i][j] == 0) {
-                    ans[i + 1][j] = 0;
-                    if (s[i] == t[j]) ans[i + 1][j + 1] = 0;
-                }
-            }
-            ans[i][n] = 1;
-        }
-        if (s[m - 1] == t[n - 1]) ans[m - 1][n - 1] = 1;
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = 0; j < n; ++j) {
-                if (ans[i][j] == 0) {
-                    if (s[i] == t[j]) ans[i][j] = ans[i + 1][j] + ans[i + 1][j + 1];
-                    else ans[i][j] = ans[i + 1][j];
-                }
+        for (int i = 1; i < ls + 1; ++i) {
+            for (int j = 1; j < lt + 1; ++j) {
+                if (s[i] == t[j]) dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                else dp[i][j] = dp[i - 1][j];
             }
         }
-        return ans[0][0];
+        return dp[ls][lt];
     }
 };
+
+// Solution
+// dp[i][j] := num of distinct subsequences of s[0: i] to generate t[0 : j]
+// if (s[i] != t[j]) dp[i][j] = dp[i - 1][j]
+// else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+// initial: dp[0][0] = s[0] == t[0] ? 1 : 0
+//          dp[i][0] = s[i] == t[0] ? 1 + dp[i - 1][0] : dp[i - 1][0]
+// return -> dp[s.length()][t.length()]
