5082e8d22830c405d0d46e7de703c755452eb0b9
Siping Wang <wangsiping1997@gmail.com>
2022-11-16 19:43:36 -0800

    Interval List Intersections

--- 986.cpp
+++ 986.cpp
@@ -2,67 +2,42 @@
 // Return the intersection of these two interval lists.
 // (Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
 
-#include <vector> 
-using namespace std; 
+#include <vector>
+using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector< vector<int> > intervalIntersection(vector< vector<int> >& A, vector< vector<int> >& B) {
-        vector< vector<int> > ans;
-        vector<int> tempans;
-        int m = A.size();
-        int n = B.size();
+    vector<vector<int>> intervalIntersection(vector<vector<int>> &firstList, vector<vector<int>> &secondList)
+    {
+        int m = firstList.size();
+        int n = secondList.size();
         int i = 0, j = 0;
-        while (i < m && j < n) {
-            if (A[i][0] < B[j][0]) {
-                if (A[i][1] >= B[j][0]) {
-                    tempans.clear();
-                    tempans.push_back(B[j][0]);
-                    tempans.push_back(min(A[i][1], B[j][1]));
-                    ans.push_back(tempans);
-                    if (A[i][1] < B[j][1]) 
-                        i++;
-                    else if (A[i][1] > B[j][1]) 
-                        j++;
-                    else {
-                        i++;
-                        j++;
-                    }
-                } else {
-                    i++;
-                }
-            } else if (A[i][0] > B[j][0]) {
-                if (B[j][1] >= A[i][0]) {
-                    tempans.clear();
-                    tempans.push_back(A[i][0]);
-                    tempans.push_back(min(A[i][1], B[j][1]));
-                    ans.push_back(tempans);
-                    if (A[i][1] <= B[j][1]) 
-                        i++;
-                    else if (A[i][1] >= B[j][1])
-                        j++;
-                    else {
-                        i++;
-                        j++;
-                    }
-                } else {
-                    j++;
-                }
-            } else {
-                tempans.clear();
-                tempans.push_back(A[i][0]);
-                tempans.push_back(min(A[i][1], B[j][1]));
-                ans.push_back(tempans);
-                if (A[i][1] < B[j][1]) 
-                    i++;
-                else if (A[i][1] > B[j][1])
-                    j++;
-                else {
-                    i++;
-                    j++;
-                }
+        vector<vector<int>> result;
+        while (i < m && j < n)
+        {
+            vector<int> interval1 = firstList[i];
+            vector<int> interval2 = secondList[j];
+            int start = max(interval1[0], interval2[0]);
+            int end = min(interval1[1], interval2[1]);
+            if (start <= end)
+            {
+                result.push_back({start, end});
+            }
+            if (interval1[1] < interval2[1])
+            {
+                i++;
+            }
+            else if (interval1[1] > interval2[1])
+            {
+                j++;
+            }
+            else
+            {
+                i++;
+                j++;
             }
         }
-        return ans;
+        return result;
     }
 };
