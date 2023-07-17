38afe151d69f9e6e450159a1f8bf8e6aa297240a
Siping Wang <wangsiping1997@gmail.com>
2023-07-16 23:22:19 -0700

    Kth Largest Element in an Array

--- 215.cpp
+++ 215.cpp
@@ -1,20 +1,37 @@
-// Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+// Find the kth largest element in an unsorted array. Note that it is the kth
+// largest element in the sorted order, not the kth distinct element.
 
-#include <vector> 
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+#include <vector>
+using namespace std;
 
 class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        make_heap(nums.begin(), nums.begin() + k, greater<int>());
-        for (int i = k; i < nums.size(); ++i) {
-            if (nums[i] > nums[0]) {
-                pop_heap(nums.begin(), nums.begin() + k, greater<int>());
-                swap(nums[i], nums[k - 1]);
-                push_heap(nums.begin(), nums.begin() + k, greater<int>());
-            }
-        }
-        return nums[0];
+ public:
+  int findKthLargest(vector<int>& nums, int k) {
+    int n = nums.size();
+    if (n == 1) return nums[0];
+    int pivot = nums[0];
+    vector<int> left;
+    vector<int> right;
+    for (int val : nums) {
+      if (val > pivot) {
+        left.push_back(val);
+      } else if (val < pivot) {
+        right.push_back(val);
+      }
     }
+    int n_left = left.size();
+    int n_right = right.size();
+    int n_mid = n - n_left - n_right;
+    if (n_left >= k) {
+      return findKthLargest(left, k);
+    }
+    if (n_left + n_mid >= k) {
+      return pivot;
+    }
+    return findKthLargest(right, n_right - (n - k));
+  }
 };
+
+// Solution
+// Similar to fastsort, O(N)
